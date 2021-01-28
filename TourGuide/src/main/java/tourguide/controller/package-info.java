0ef510f4f6d61b1da39f57a9ab3d3628a0f5fc65
yaya <cherifi.yahia@yahoo.fr>
2021-01-28 20:50:42 +0100

    TourGuideController documented and moved to the controller package.

---
+++ TourGuide/src/main/java/tourguide/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The Controllers package.
+ * This package contains all the TourGuide controllers.
+ *
+ * @author Some TourGuide Developer
+ * @author Yahia CHERIFI
+ */
+
+package tourguide.controller;
