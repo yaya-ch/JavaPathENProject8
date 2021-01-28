249da6b0c630ee19587a44ad26133e9d3d79fb85
yaya <cherifi.yahia@yahoo.fr>
2021-01-28 20:23:07 +0100

    ExecutorService bean created in the TourGuideModule. TourGuideModule refactored and moved to the right package.

---
+++ TourGuide/src/main/java/tourguide/config/TourGuideModule.java
@@ -0,0 +1,67 @@
+package tourguide.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import gpsUtil.GpsUtil;
+import rewardCentral.RewardCentral;
+import tourguide.service.RewardsService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * TourGuide Config class.
+ * This class contains the different beans that can be used by the application
+ *
+ * @author Some TourGuide Developer
+ * @author Yahia CHERIFI
+ */
+
+@Configuration
+public class TourGuideModule {
+
+    /**
+     * The maximum number of threads that will be active to process tasks.
+     */
+    private static final int NUMBER_OF_THREADS = 100;
+
+    /**
+     * The gpsUtil bean.
+     * @return a new instance of the gpsUtil
+     */
+    @Bean
+    public GpsUtil getGpsUtil() {
+        return new GpsUtil();
+    }
+
+    /**
+     * The rewardService bean.
+     * @return a new instance of the RewardService
+     */
+    @Bean
+    public RewardsService getRewardsService() {
+        return new RewardsService(getGpsUtil(),
+                getRewardCentral(),
+                executorService());
+    }
+
+    /**
+     * The rewardCentral bean.
+     * @return a new instance of the RewardCentral
+     */
+    @Bean
+    public RewardCentral getRewardCentral() {
+        return new RewardCentral();
+    }
+
+    /**
+     * The executorService bean.
+     * @return a new instance of the ExecutorService.
+     */
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    }
+
+}
