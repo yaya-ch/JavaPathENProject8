3fee3e26e5c0f522c6192decac4bffc0821a3d8a
yaya <cherifi.yahia@yahoo.fr>
2021-01-31 19:02:53 +0100

    Tracker class refactored. package info created in the tracker package.

--- TourGuide/src/main/java/tourguide/tracker/Tracker.java
+++ TourGuide/src/main/java/tourguide/tracker/Tracker.java
@@ -10,52 +10,88 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import tourguide.service.TourGuideService;
-import tourguide.user.User;
+import tourguide.domain.User;
+
+/**
+ * The Tracker class.
+ *
+ * @author Some TourGuide Developer
+ */
 
 public class Tracker extends Thread {
-	private Logger logger = LoggerFactory.getLogger(Tracker.class);
-	private static final long trackingPollingInterval = TimeUnit.MINUTES.toSeconds(5);
-	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
-	private final TourGuideService tourGuideService;
-	private boolean stop = false;
 
-	public Tracker(TourGuideService tourGuideService) {
-		this.tourGuideService = tourGuideService;
-		
-		executorService.submit(this);
-	}
-	
-	/**
-	 * Assures to shut down the Tracker thread
-	 */
-	public void stopTracking() {
-		stop = true;
-		executorService.shutdownNow();
-	}
-	
-	@Override
-	public void run() {
-		StopWatch stopWatch = new StopWatch();
-		while(true) {
-			if(Thread.currentThread().isInterrupted() || stop) {
-				logger.debug("Tracker stopping");
-				break;
-			}
-			
-			List<User> users = tourGuideService.getAllUsers();
-			logger.debug("Begin Tracker. Tracking " + users.size() + " users.");
-			stopWatch.start();
-			users.forEach(u -> tourGuideService.trackUserLocation(u));
-			stopWatch.stop();
-			logger.debug("Tracker Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds."); 
-			stopWatch.reset();
-			try {
-				logger.debug("Tracker sleeping");
-				TimeUnit.SECONDS.sleep(trackingPollingInterval);
-			} catch (InterruptedException e) {
-				break;
-			}
-		}
-		
-	}
+    /**
+     * Class logger.
+     */
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Tracker.class);
+
+    /**
+     * The tracking interval.
+     */
+    private static final long TRACKING_POLLING_INTERVAL =
+            TimeUnit.MINUTES.toSeconds(5);
+
+    /**
+     * The executor service.
+     */
+    private final ExecutorService executorService =
+            Executors.newSingleThreadExecutor();
+
+    /**
+     * The TourGuideService.
+     */
+    private final TourGuideService tourGuideService;
+
+    /**
+     * The boolean stop.
+     */
+    private boolean stop = false;
+
+    /**
+     * Class constructor.
+     * @param pTourGuideService the TourGuideService
+     */
+    public Tracker(final TourGuideService pTourGuideService) {
+        this.tourGuideService = pTourGuideService;
+        executorService.submit(this);
+    }
+
+    /**
+    * Assures to shut down the Tracker thread.
+    */
+    public void stopTracking() {
+        stop = true;
+        executorService.shutdownNow();
+    }
+
+    /**
+     * Overriding the run method of the Thread class.
+     */
+    @Override
+    public void run() {
+        StopWatch stopWatch = new StopWatch();
+        while (true) {
+            if (Thread.currentThread().isInterrupted() || stop) {
+                LOGGER.debug("Tracker stopping");
+                break;
+            }
+
+            List<User> users = tourGuideService.getAllUsers();
+            LOGGER.debug("Begin Tracker. Tracking " + users.size() + " users.");
+            stopWatch.start();
+            users.forEach(tourGuideService::trackUserLocation);
+            stopWatch.stop();
+            LOGGER.debug("Tracker Time Elapsed: "
+                    + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime())
+                    + " seconds.");
+            stopWatch.reset();
+            try {
+                LOGGER.debug("Tracker sleeping");
+                TimeUnit.SECONDS.sleep(TRACKING_POLLING_INTERVAL);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
 }
