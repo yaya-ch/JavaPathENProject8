1c658725a7d0a0c86d68a8e3b2271b3a33bc796b
yaya <cherifi.yahia@yahoo.fr>
2021-02-18 11:14:13 +0100

    doc added to the DTO class. package-info created inside the dto package.

--- TourGuide/src/main/java/tourguide/dto/CurrentLocationDTO.java
+++ TourGuide/src/main/java/tourguide/dto/CurrentLocationDTO.java
@@ -5,42 +5,86 @@ import gpsUtil.location.Location;
 import java.util.UUID;
 
 /**
+ * A class that holds some current location related information.
+ * <p>
+ * Used mainly to get the users' current location
  *
+ * @author Yahia CHERIFI
  */
 public class CurrentLocationDTO {
 
+    /**
+     * the user's id.
+     */
     private UUID userId;
+
+    /**
+     * The user's location.
+     */
     private Location location;
 
+    /**
+     * Class constructor.
+     */
     public CurrentLocationDTO() {
     }
 
-    public CurrentLocationDTO(UUID userId, Location location) {
-        this.userId = userId;
-        this.location = location;
+    /**
+     * Class constructor.
+     *
+     * @param pUserId   the use's id
+     * @param pLocation the user's location
+     */
+    public CurrentLocationDTO(final UUID pUserId, final Location pLocation) {
+        this.userId = pUserId;
+        this.location = pLocation;
     }
 
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
     public UUID getUserId() {
         return userId;
     }
 
-    public void setUserId(UUID userId) {
-        this.userId = userId;
+    /**
+     * Sets user id.
+     *
+     * @param pUserId the user id
+     */
+    public void setUserId(final UUID pUserId) {
+        this.userId = pUserId;
     }
 
+    /**
+     * Gets location.
+     *
+     * @return the location
+     */
     public Location getLocation() {
         return location;
     }
 
-    public void setLocation(Location location) {
-        this.location = location;
+    /**
+     * Sets location.
+     *
+     * @param pLocation the location
+     */
+    public void setLocation(final Location pLocation) {
+        this.location = pLocation;
     }
 
+    /**
+     * ToString method.
+     * @return CurrentLocation in a string format
+     */
     @Override
     public String toString() {
-        return "CurrentLocationDTO{" +
-                "userId=" + userId +
-                ", location=" + location +
-                '}';
+        return "CurrentLocationDTO{"
+                + "userId=" + userId
+                + ", location=" + location
+                + '}';
     }
 }
